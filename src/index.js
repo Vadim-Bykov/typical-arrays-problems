f32967ace745dade740f26263504c8e3a7a83392
Vadim-Bykov <bvntaev@gmail.com>
2020-09-29 20:59:17 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (Array.isArray(array)) {
+        const res = Math.min(...array)
+        if (res === Infinity || res == 0) {
+            return 0
+        }
+        return res
+    } else return 0
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (Array.isArray(array)) {
+        const res = Math.max(...array)
+        if (res === -Infinity || res == 0) {
+            return 0
+        }
+        return res
+    } else return 0
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (Array.isArray(array) && array.length > 0) {
+        const res = array.reduce((total, num) => total += num, 0) / array.length
+        return res
+    } else return 0
 }
